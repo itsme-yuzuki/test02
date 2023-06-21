2444ea7729a0038b0d63bcc23170d3361f3924ac
id <id@AQ-0038>
2023-06-21 16:21:52 +0900

    お返事！

--- src/test02/Sample01.java
+++ src/test02/Sample01.java
@@ -5,6 +5,7 @@ public class Sample01 {
 	public static void main(String[] args) {
 		System.out.println("こんにちは！よろしく！！");
 		System.out.println("こちらこそ！よろしく！！");
+		System.out.println("上手くいってよかった！奈に作ろうかねえ～笑");
 	}
 
 }
